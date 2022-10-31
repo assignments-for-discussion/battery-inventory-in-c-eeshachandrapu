6692c57b28d08b19f94505cacadaf20f87002968
eeshachandrapu <117082044+eeshachandrapu@users.noreply.github.com>
2022-10-31 16:58:24 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -15,12 +15,22 @@ struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
 void testBucketingByNumberOfCycles() {
   const int chargeCycleCounts[] = {100, 300, 500, 600, 900, 1000};
   const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
+  int i;
   printf("Counting batteries by usage cycles...\n");
   struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
-  assert(counts.lowCount == 2);
-  assert(counts.mediumCount == 3);
-  assert(counts.highCount == 1);
+  for(i=0;i<numberOfBatteries;i++)
+  {
+    if(chargeCycleCounts[i]<410)
+      counts.lowCount++;
+    else if(chargeCycleCounts[i]>=410 && chargeCycleCounts[i]<950)
+      counts.mediumCount++;
+    else if(chargeCycleCounts[i]>=950)
+      counts.highCount++;
+  }
   printf("Done counting :)\n");
+  printf("Number of low usage batteries: %d\n",counts.lowCount);
+  printf("Number of medium usage batteries: %d\n",counts.mediumCount);
+  printf("Number of high usage batteries: %d\n",counts.highCount);
 }
 
 int main() {
